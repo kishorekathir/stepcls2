91017deead52cd416becf71012390b369a555704
kishore kathir R <kishore19752008@gmail.com>
2026-04-12 12:24:11 +0530

    Add files via upload

--- helloapp.java
+++ helloapp.java
@@ -1,8 +1,11 @@
 public class helloapp{
 	public static void main(string[] args){
-		string name = "world";
+		string name;
 		if (args.length>0){
-			name=args[0];
+			name=string.join(",",args);
+		}
+		else{
+			name="world";
 		}
 		system.out.println("hello," +name+ "!");
 	}
